0a4d967a0d8e0fdeeeda23ffb5a5d787d55f1ff0
nemudrena <an184@yandex.ru>
2019-10-26 22:14:45 +0300

    update

---
+++ app/src/main/java/com/example/myapp/ActivityWays.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityWays extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.window_ways);
+    }
+}
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainwindow);
+    }
+
+    public void to_window_ways(View view){
+        Intent intent = new Intent(this, ActivityWays.class);
+        startActivity(intent);
+    }
+
+    public void to_window_pics(View view){
+        Intent intent = new Intent(this, ActivityPics.class);
+        startActivity(intent);
     }
 }
